51487ec4b0751fd0b59d0861aeb0ec9ef2969241
Jeremy <ag.cyho@gmail.com>
2024-02-07 14:33:56 +0800

    feat: add vue-finder

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import VueFinder from './components/VueFinder.vue';
+</script>
+
+<template>
+  <VueFinder/>
+</template>
+
---
+++ src/components/VueFinder.vue
@@ -0,0 +1,7 @@
+<script setup>
+const request = "https://httpbin.org/post"
+</script>
+
+<template>
+    <vue-finder id='my_vuefinder' :request="request"></vue-finder>
+</template>
