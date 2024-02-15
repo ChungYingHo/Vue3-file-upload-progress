7b45ba4f028fafab273d89b81cc437b28f9a3edf
Jeremy <ag.cyho@gmail.com>
2024-02-15 11:18:22 +0800

    need to fix

--- src/App.vue
+++ src/App.vue
@@ -9,31 +9,36 @@ const tab = ref(null)
 </script>
 
 <template>
-    <v-card style="width: 50vw;height: 75vh;">
+    <v-card style="min-width: 50vw;min-height: 75vh;width: fit-content;height: fit-content;">
       <v-tabs
         v-model="tab"
         color="deep-purple-accent-4"
         align-tabs="end"
       >
-        <v-tab :value="1">Native JS & axios</v-tab>
-        <v-tab :value="2">VueFinder</v-tab>
-        <v-tab :value="3">Uppy</v-tab>
+        <v-tab :value="1">ReadableStream</v-tab>
+        <v-tab :value="2">Axios</v-tab>
+        <v-tab :value="3">VueFinder</v-tab>
+        <v-tab :value="4">Uppy</v-tab>
       </v-tabs>
       <v-window v-model="tab">
         <v-window-item :value="1">
           <v-container fluid class="d-flex justify-center align-center">
             <Stream v-if="tab === 1"/>
-            <Axios v-if="tab === 1"/>
           </v-container>
         </v-window-item>
         <v-window-item :value="2">
-          <v-container fluid>
-            <VueFinder v-if="tab === 2"/>
+          <v-container fluid class="d-flex justify-center align-center">
+            <Axios v-if="tab === 2"/>
           </v-container>
         </v-window-item>
         <v-window-item :value="3">
+          <v-container fluid>
+            <VueFinder v-if="tab === 3"/>
+          </v-container>
+        </v-window-item>
+        <v-window-item :value="4">
           <v-container fluid class="d-flex justify-center align-center">
-            <Uppy v-if="tab === 3"/>
+            <Uppy v-if="tab === 4"/>
           </v-container>
         </v-window-item>
       </v-window>
